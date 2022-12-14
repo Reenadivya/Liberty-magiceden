9bf7a0ead96b90fb8e5635f715f5631727057b91
Reena Divya <107604182+Reenadivya@users.noreply.github.com>
2022-12-14 12:59:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ btn.addEventListener("click", function () {
   parseTxn(inputString).then((data) => {
     const mintAcc = data.events.nft.nfts[0].mint;
     const saleAmt = data.events.nft.amount;
-    console.log(saleAmt);
-    console.log(data);
-    console.log(data.description);
+    const nativeTrf = data.nativeTransfers;
+  
     let arr = [];
     arr.push(data);
-    console.log(arr);
+
     let tableData = "";
     arr.map((values) => {
-      console.log(values);
       tableData = `
       <p>Description: ${values.description}</p>
       <p>Type: ${values.type}</p>
@@ -27,42 +25,66 @@ btn.addEventListener("click", function () {
     document.getElementById("nft__container").innerHTML = tableData;
 
     const nftAddresses = [mintAcc];
-    console.log(nftAddresses); // Monkes
+
     const getMetadata = async (mintaddress) => {
       const { data } = await axios.post(
         `https://api.helius.xyz/v0/tokens/metadata?api-key=${apiKey}`,
-        { mintAccounts: nftAddresses }
+        { mintAccounts: mintaddress }
       );
       console.log(data);
+      let newTable = "";
+      data.map((values) => {
+        console.log(values);
+        newTable = `
+      <p>Royalty Fee: ${values.onChainData.data.sellerFeeBasisPoints}</p>
+      `;
+      });
+      document.getElementById("nft__container2").innerHTML = newTable;
       console.log(
         `Royalty Fee: ${data[0].onChainData.data.sellerFeeBasisPoints}`
       );
+
       const creators = data[0].onChainData.data.creators;
-      const creatorsArr = [];
-      for (let i = 0; i < creators.length; i++) {
-        creatorsArr.push(creators[i]);
+      let thirdtableData = "";
+      for (const creator of creators) {
+        if (creator.share > 0) {
+          console.log(`Checking paymentsfor creator ${creator.address}`);
+          const royalitypayment = nativeTrf.filter(
+            (trf) => trf.toUserAccount === creator.address
+          );
+          console.log(royalitypayment);
+          thirdtableData = `
+              <p>Checking paymentsfor creator ${creator.address}</p>
+            `;
+
+          if (royalitypayment.length > 0) {
+            const payentAmt = royalitypayment[0].amount;
+            console.log(`${payentAmt}was paid to ${creator.address}`);
+          } else {
+            console.log(`Royalties not paid to ${creator.address}`);
+            thirdtableData += `<p>Royalties not paid to ${creator.address}</p>`;
+          }
+        }
       }
+      document.getElementById("nft__container3").innerHTML = thirdtableData;
     };
 
-    getMetadata();
+    getMetadata(nftAddresses);
   });
 });
 
-const apiKey = //-- ENTER YOUR API KEY HERE--//
-async function parseTxn(txnStr) {
-  const { data } = await axios.post(
-    `https://api.helius.xyz/v0/transactions/?api-key=${apiKey}`,
-    {
-      transactions: [txnStr],
-    }
-  );
-  return data[0];
-}
+const apiKey = //ENTER YOUR API KEY HERE//;
+  async function parseTxn(txnStr) {
+    const { data } = await axios.post(
+      `https://api.helius.xyz/v0/transactions/?api-key=${apiKey}`,
+      {
+        transactions: [txnStr],
+      }
+    );
+    return data[0];
+  };
 
-const nftAddresses = [
-  "BAAzgRGWY2v5AJBNZNFd2abiRXAUo56UxywKEjoCZW2",
-  "8s6kQUZfdm7GSaThAcsmSs56wMinXrbk6SdNVngutrz5",
-]; // Monkes
+
 const getMetadata = async (mintaddress) => {
   const { data } = await axios.post(
     `https://api.helius.xyz/v0/tokens/metadata?api-key=${apiKey}`,
@@ -70,4 +92,3 @@ const getMetadata = async (mintaddress) => {
   );
   console.log("metadata: ", data);
 };
-getMetadata();
